bb0d394e6bd7e0e342aaf2cb97d98c4842d8f549
hiromuota166 <otahiromu166@gmail.com>
2023-12-05 00:46:51 +0900

    if文に変更

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import time
 from linenotify import send_line_notify
+import schedule
 
 def run():
     return send_line_notify("これはテストです")
 
-while True:
-    run()
+# 火曜日の午前1時に実行
+schedule.every().tuesday.at("01:00").do(run)
+
+if __name__ == "__main__":
+    schedule.run_pending()
     time.sleep(100)
