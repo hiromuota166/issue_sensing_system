6308ac4d7d234ebb6cce8b62c87f75e643d30e50
hiromuota166 <otahiromu166@gmail.com>
2023-12-05 01:11:11 +0900

    cron job

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-import time
 from linenotify import send_line_notify
-import schedule
 
 def run():
-    return send_line_notify("1時だよ")
-
-# 火曜日の午前1時に実行
-schedule.every().tuesday.at("01:00").do(run)
+    return send_line_notify("cron job")
 
 if __name__ == "__main__":
-    schedule.run_pending()
-    time.sleep(100)
+    run()
